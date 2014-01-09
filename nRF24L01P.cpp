9984c6a1f0b288762ecab849c8624f85374bf16f
Lorenzo Fontana <lord.fontana@gmail.com>
2014-01-09 18:58:22 +0100

    Creati metodi set_register power_up/down set_transmit/receive mode piu la struttura dati mode
    Signed-off-by: Lorenzo Fontana <lord.fontana@gmail.com>

--- nRF24L01P.cpp
+++ nRF24L01P.cpp
@@ -9,9 +9,30 @@
 #include <miosix.h>
 #include <miosix/kernel/scheduler/scheduler.h>
 
-//NRF24L01P Macro
-#define NRF24L01P_CMD_RD_REG 0x00
-#define NRF24LO1P_RG_BITMASK 0x1f
+//NRF24L01P Macro 
+//Command
+#define NRF24L01P_CMD_RD_REG     0x00
+#define NRF24L01P_CMD_WT_REG     0x01
+
+//bitmask and register address
+#define NRF24LO1P_REG_ADDR_BITMASK    0x1f
+#define NRF24L01P_REG_CONF       0x00
+
+//set data to register
+#define NRF24L01P_PRIM_RX        (1<<0)
+#define NRF24L01P_PWR_UP         (1<<1)
+
+//time
+#define NRF24L01P_TPD2STBY       2000  //2mS
+#define NRF24L01P_TPECE2CSN      4     //4uS
+
+typedef enum {
+    NRF24L01P_UNKNOWN_MODE,
+    NRF24L01P_POWER_DOWN_MODE,
+    NRF24L01P_STANDBY_MODE,
+    NRF24L01P_RX_MODE,
+    NRF24L01P_TX_MODE,
+} NRF24L01P_mode;       
 
 using namespace miosix;
 
@@ -31,12 +52,7 @@ typedef Gpio<GPIOD_BASE,12> greenLed;
 
 
 nRF24L01P::nRF24L01P() {
-}
-
-nRF24L01P::nRF24L01P(const nRF24L01P& orig) {
-}
-
-nRF24L01P::~nRF24L01P() {
+    spi = new spi_driver();
     MISO::mode(Mode::ALTERNATE);
     MISO::alternateFunction(5);
     MOSI::mode(Mode::ALTERNATE); 
@@ -51,9 +67,65 @@ nRF24L01P::~nRF24L01P() {
     
 }
 
-void nRF24L01P::init() {
-    spi = new spi_driver();
+nRF24L01P::nRF24L01P(const nRF24L01P& orig) {
 }
+
+nRF24L01P::~nRF24L01P() {
+   
+}
+
+/**
+ * power up the module
+ */
+void nRF24L01P::power_up() {
+    //I get the current config and I add the power up bit then I write it back 
+    int current_config = get_register(NRF24L01P_REG_CONF); 
+    current_config |= NRF24L01P_PWR_UP;
+    set_register(NRF24L01P_REG_CONF,current_config);
+    usleep(NRF24L01P_TPD2STBY);
+    mode=NRF24L01P_STANDBY_MODE;
+}
+
+void nRF24L01P::power_down() {
+    int current_config = get_register(NRF24L01P_REG_CONF);
+    current_config &= ~NRF24L01P_PWR_UP;
+    set_register(NRF24L01P_REG_CONF,current_config);
+    usleep(NRF24L01P_TPD2STBY);
+    mode = NRF24L01P_POWER_DOWN_MODE;
+}
+
+void nRF24L01P::set_receive_mode(){
+    
+    if (mode==NRF24L01P_POWER_DOWN_MODE){
+        power_up();
+    }
+    int cur_config = get_register(NRF24L01P_REG_CONF);
+    cur_config |= NRF24L01P_PRIM_RX;
+    set_register(NRF24L01P_REG_CONF,cur_config);
+    if (CE.value()==0){
+        CE.high();
+    }
+    usleep(NRF24L01P_TPECE2CSN);
+    mode = NRF24L01P_RX_MODE;
+   
+}
+
+void nRF24L01P::set_transmit_mode(){
+    if (mode==NRF24L01P_POWER_DOWN_MODE){
+        power_up();
+    }
+    int cur_config = get_register(NRF24L01P_REG_CONF);
+    cur_config &= ~NRF24L01P_PRIM_RX;
+    set_register(NRF24L01P_REG_CONF,cur_config);
+    if (CE.value()==0){
+        CE.high();
+    }
+    usleep(NRF24L01P_TPECE2CSN);
+    mode = NRF24L01P_TX_MODE;
+    
+}
+
+
 void nRF24L01P::transmit(int num_passi){
 
 }
@@ -61,18 +133,30 @@ void nRF24L01P::transmit(int num_passi){
 int nRF24L01P::receive(){
     return 0;
 }
+/**
+ * function allowes to set a register to a particular value
+ * @param addr_registro address of the register
+ * @param data_registro data to set the register
+ */
+void nRF24L01P::set_register(int addr_registro,int data_registro){
+        int old_ce =CE.value()  //save the CE value    
+        CE.low();               //in order to change value of register the module has to be in StandBy1 mode
+        int status = spi.spi_write(NRF24L01P_CMD_WT_REG |(addr_registro & NRF24LO1P_REG_ADDR_BITMASK)); //command to write the at correct address of register
+        spi.spi_write(data_registro & 0xff);    //data used to set the register
+        CE.value()=old_ce;                      //restore old ce value
+        usleep(NRF24L01P_TPECE2CSN);            //sleep to apply ce value change
+        
+
+}
 
 int  nRF24L01P::get_register(int registro){
-    int command = NRF24L01P_CMD_RD_REG | (registro & NRF24LO1P_RG_BITMASK);
+    int command = NRF24L01P_CMD_RD_REG | (registro & NRF24LO1P_REG_ADDR_BITMASK);
     int result;
     CS::low();
-    spi->spi_write(command);   
-    result = spi->spi_Receive();
+    spi.spi_write(command);   
+    result = spi.spi_Receive();
     CS::high();
-    return result;
-    
-    
-    
+    return result;   
 }
 
 
