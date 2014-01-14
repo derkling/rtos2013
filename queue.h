6b15900cfcdf7a4b0adec7823f9166a6f1bcbf23
Stefano Bielli <stefano.bielli@gmail.com>
2014-01-14 18:30:46 +0100

    Modifica dell'header queue.h
    Signed-off-by: Stefano Bielli <stefano.bielli@gmail.com>

--- queue.h
+++ queue.h
@@ -1,12 +1,22 @@
 #ifndef QUEUE_H
 #define	QUEUE_H
+#define QUEUE_LENGHT 1024
 
+//TYPEDEF
+typedef struct{
+    char buffer[QUEUE_LENGHT]; //elementi della coda
+    char* head = buffer; //testa della coda.
+    char* next = head; //prossimo elemento libero della coda
+    bool isFull = false; //indica se la coda ha elementi
+}queue;
+
+//FUNZIONI
 /*!@brief Aggiunge un dato alla coda.
  * @param data: stringa contenente il dato da inserire in coda.
  * @param queue: coda da gestire.
  * @retval Ritorna -1 se la coda è piena.
  */
-int addData(char* data, char* queue);
+int addData(char* data, queue q);
 
 /*!@brief Serve la coda. Si consiglia di utilizzare prima la funzione isEmpty() 
  *        per controllare se la coda ha elementi.
@@ -14,13 +24,7 @@ int addData(char* data, char* queue);
  * @retval Ritorna il valore del primo dato da servire in coda, se non ci sono 
  *         elementi in coda ritorna NULL.
  */
-char* enqueue(char* queue);
-
-/*!@brief Controlla se ci sono elementi in coda.
- * @param queue: coda da gestire.
- * @retval Se ci sono elementi in coda ritorna false, altrimenti true.
- */
-bool isEmpty(char* queue);
+char* enqueue(queue q);
 
 #endif	/* QUEUE_H */
 
