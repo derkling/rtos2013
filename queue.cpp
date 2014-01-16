1434f49f1b6f6e4d6fddb43b1bfe2c006ee32077
Stefano Bielli <stefano.bielli@gmail.com>
2014-01-16 16:15:07 +0100

    Corretti bug
    Signed-off-by: Stefano Bielli <stefano.bielli@gmail.com>

--- queue.cpp
+++ queue.cpp
@@ -4,75 +4,73 @@
 
 //FUNZIONI
 
-queue_t queueInizializer(queue_t queue){
+void queueInizializer(queue_t* queue){
     //inizializzo l'head
-    queue.head = queue.buffer;
+    queue->head = queue->buffer;
     //inizializzo il next
-    queue.next = queue.head;
+    queue->next = queue->head;
     //inizializzo la quantita' di spazio libero
-    queue.freeSpace = QUEUE_LENGHT;
+    queue->freeSpace = QUEUE_LENGHT;
     //ritorno queue inizializzato
-    return queue;
 }
 
-int addData (char* data, queue_t queue){
+int addData (char* data, queue_t* queue){
     
     int size = strlen(data);
-    if ((queue.freeSpace != 0) || (size + 1 > queue.freeSpace)){
+    if ((queue->freeSpace == 0) || (size + 1 > queue->freeSpace)){
         //coda piena
         return -1;
     }
     
-    for (int i = 0; data[i] != '\0'; i++){
-        *(queue.next) = data[i];
-        queue.freeSpace--;
-        //controllare la correttezza
-        if (queue.next + 1 == &(queue.buffer[QUEUE_LENGHT])){
-            queue.next = queue.buffer;
+    for (int i = 0; i < size; i++){
+        *(queue->next) = data[i];
+        queue->freeSpace--;
+        if (queue->next + 1 == &(queue->buffer[QUEUE_LENGHT])){
+            queue->next = queue->buffer;
         } else {
-            queue.next++;
+            queue->next++;
         }
     }
     
-    *(queue.next) = SEPARATORE;
-    queue.freeSpace--;
-    queue.next++;
+    *(queue->next) = SEPARATORE;
+    queue->freeSpace--;
+    queue->next++;
     
     return 0;
 }
 
-char* enqueue(queue_t queue){
+char* enqueue(queue_t* queue){
     int dataSize = 1;
-    char* pnt = queue.head;
+    char* pnt = queue->head;
     while(*(pnt) != SEPARATORE){
         dataSize++;
-        if (pnt == &(queue.buffer[QUEUE_LENGHT])){
-            pnt = queue.buffer;
+        if (pnt +1 == &(queue->buffer[QUEUE_LENGHT])){
+            pnt = queue->buffer;
         } else {
             pnt++;
         }
     }
     char data[dataSize];
-    for (int i = 0; *(queue.head) != SEPARATORE; i++){
-        data[i] = *(queue.head);
-        queue.freeSpace++;
-        if (queue.head == &(queue.buffer[QUEUE_LENGHT])){
-            queue.head = queue.buffer;
+    for (int i = 0; *(queue->head) != SEPARATORE; i++){
+        data[i] = *(queue->head);
+        queue->freeSpace++;
+        if (queue->head +1 == &(queue->buffer[QUEUE_LENGHT])){
+            queue->head = queue->buffer;
         } else {
-            queue.head++;
+            queue->head++;
         }
     }
-    queue.head++;
-    queue.freeSpace++;
+    queue->head++;
+    queue->freeSpace++;
     data[dataSize - 1] = '\0';
     
     return data;
 }
 
-bool queueIsEmpty(queue_t queue){
-    if (queue.freeSpace != 0){
-        return false;
-    } else {
+bool queueIsEmpty(queue_t* queue){
+    if (queue->freeSpace == QUEUE_LENGHT){
         return true;
+    } else {
+        return false;
     }
 }
--- queue.h
+++ queue.h
@@ -16,26 +16,26 @@ typedef struct{
 /*!@brief Crea un nuovo dato tipo queue_t correttamente inizializzato.
  * @retval Ritorna l'elemento queue_t correttamente inizializzato.
  */
-queue_t queueInizializer(queue_t queue);
+void queueInizializer(queue_t* queue);
 
 /*!@brief Aggiunge un dato alla coda.
  * @param data: stringa contenente il dato da inserire in coda.
  * @param queue: coda da gestire.
  * @retval Ritorna -1 se la coda è piena, 0 altrimenti.
  */
-int addData(char* data, queue_t queue);
+int addData(char* data, queue_t* queue);
 
-/*!@brief Serve la coda. Si consiglia di utilizzare prima la funzione isEmpty() 
- *        per controllare se la coda ha elementi.
+/*!@brief Serve la coda. Si consiglia di utilizzare prima la funzione 
+ *        queueIsEmpty() per controllare se la coda ha elementi.
  * @param queue: coda da gestire.
  * @retval Ritorna il valore del primo dato da servire in coda, se non ci sono 
  *         elementi in coda ritorna NULL.
  */
-char* enqueue(queue_t queue);
+char* enqueue(queue_t* queue);
 
 /*!@brief Controlla se la coda è vuota.
  * @param queue: coda da gestire.
  * @retval Ritorna true se la coda è vuota, false altrimenti.
  */
-bool queueIsEmpty(queue_t queue);
+bool queueIsEmpty(queue_t* queue);
 #endif	/* QUEUE_H */
