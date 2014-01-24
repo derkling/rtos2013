4ae77f079b6df03c8d61bc51d31ae6e5ade138a9
Marco Mezzanotte <mezzanotte.marco@gmail.com>
2014-01-24 19:49:14 +0100

    main test for multiple play delays

--- progetto/main.cpp
+++ progetto/main.cpp
@@ -1,10 +1,6 @@
 
 #include "player.h"
 #include "prova_16.h"
-#include "tre.h"
-
-
-#include "sad_trombone.h"
 
 void delay()
 {
@@ -14,24 +10,7 @@ void delay()
 
 int main()
 {
-    
-/*
-	unsigned char res[prova_16_bin_len + tre_bin_len+1];
-
-	int i;
-	for(i = 0; i< tre_bin_len; i++){
-        	res[i] = tre_bin[i];
-       		// printf("\nCIAO %c", res[i]);
-  	  }
-    
-  	  for(i = tre_bin_len; i< tre_bin_len+prova_16_bin_len; i++){
-      		  res[i] = prova_16_bin[i-tre_bin_len];
-        	// printf("\nCIAO2 %c", res[i]);
- 	   }
-
-	*/
-        //ADPCMSound sound2(num_441_bin,num_441_bin_len);
-      
+   
 	  ADPCMSound sound2(prova_16_bin,prova_16_bin_len);
 	Player::instance().play(sound2, -20);
 	Player::instance().play(sound2, -20);
